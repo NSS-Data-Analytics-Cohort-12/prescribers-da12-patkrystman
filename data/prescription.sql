a601661467106374d53f68dd41d771cf26cf9dd1
Patrick K <78283766+patkrystman@users.noreply.github.com>
2024-06-15 11:33:59 -0500

    prescription.2

--- data/prescription.sql
+++ data/prescription.sql
@@ -92,31 +92,32 @@
 -- 4. 
 --     a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs. **Hint:** You may want to use a CASE expression for this. See https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-case/ 
 
--- select drug.drug_name, 
--- 	drug.drug_name as drug_type,
--- 	case drug.drug_name
--- 		when 'opioid_drug_flag = 'Y'' then 'opioid'
--- 		when 'antibiotic_drug_flag = 'Y'' then 'antibiotic'
--- 		else
--- 			'neither'
--- 		end drug_type_description
--- from drug
-
--- select drug_name, 
--- 	case drug_name
+-- select 
+-- 	drug_name,
+-- 	case
 -- 		when opioid_drug_flag = 'Y' then 'opioid'
 -- 		when antibiotic_drug_flag = 'Y' then 'antibiotic'
 -- 		else 'neither'
--- 	end drug_type_descrition
+-- 	end drug_type
 -- from drug
+-- order by drug_type
 
 
+--     b. Building off of the query you wrote for part a, determine 
+-- 	whether more was spent (total_drug_cost) on opioids or on antibiotics. 
+-- 	Hint: Format the total costs as MONEY for easier comparision.
 
-
-
---     b. Building off of the query you wrote for part a, determine whether more was spent (total_drug_cost) on opioids or on antibiotics. Hint: Format the total costs as MONEY for easier comparision.
-
-
+-- select sum(total_drug_cost) ::MONEY,
+-- 	case
+-- 		when opioid_drug_flag = 'Y' then 'opioid'
+-- 		when antibiotic_drug_flag = 'Y' then 'antibiotic'
+-- 		else 'neither'
+-- 	end drug_type
+-- from drug
+-- inner join prescription
+-- 	using(drug_name)
+-- group by drug_type
+-- order by sum(total_drug_cost) desc
 
 
 
@@ -142,24 +143,18 @@
 -- answer_'largest, 32820, 937847', 'smallest, 34980, 8773'
 
 
---     c. What is the largest (in terms of population) county which is not included in a CBSA? Report the county name and population.
-
--- select *
--- from cbsa
--- inner join population
--- using(fipscounty)
--- inner join fips_county
--- using(fipscounty)
--- order by population desc
+--     c. What is the largest (in terms of population) county which is not included in a CBSA? 
+-- 		Report the county name and population.
 
 -- select *
 -- from population
--- full join fips_county
+-- 	left join cbsa
 -- using(fipscounty)
--- inner join cbsa
+-- 	left join fips_county
 -- using(fipscounty)
--- order by population.fipscounty desc
--- limit 100
+-- order by population desc
+
+-- answer_'county name Sevier, population 95523'
 
 -- 6. 
 --     a. Find all rows in the prescription table where total_claims is at least 3000. Report the drug_name and the total_claim_count.
@@ -184,7 +179,8 @@
 
 
 
---     c. Add another column to you answer from the previous part which gives the prescriber first and last name associated with each row.
+--     c. Add another column to you answer from the previous part which gives the 
+--		prescriber first and last name associated with each row.
 
 -- select p1.drug_name, 
 -- 	p1.total_claim_count, 
@@ -201,29 +197,135 @@
 
 
 
--- 7. The goal of this exercise is to generate a full list of all pain management specialists in Nashville and the number of claims they had for each opioid. **Hint:** The results from all 3 parts will have 637 rows.
+-- 7. The goal of this exercise is to generate a full list of all pain management specialists 
+--		in Nashville and the number of claims they had for each opioid. 
+--		**Hint:** The results from all 3 parts will have 637 rows.
 
---     a. First, create a list of all npi/drug_name combinations for pain management specialists (specialty_description = 'Pain Management) in the city of Nashville (nppes_provider_city = 'NASHVILLE'), where the drug is an opioid (opiod_drug_flag = 'Y'). **Warning:** Double-check your query before running it. You will only need to use the prescriber and drug tables since you don't need the claims numbers yet.
+--     a. First, create a list of all npi/drug_name combinations for 
+--		pain management specialists (specialty_description = 'Pain Management) in the 
+--		city of Nashville (nppes_provider_city = 'NASHVILLE'), where the drug is an 
+--		opioid (opiod_drug_flag = 'Y'). **Warning:** Double-check your query before running it. 
+--		You will only need to use the prescriber and drug tables since you don't need the claims numbers yet.
 
 
 -- select drug_name, specialty_description, nppes_provider_city, opioid_drug_flag
 -- from prescriber
 -- cross join drug
--- 	where prescriber.specialty_description = 'Pain Management'
+	-- where prescriber.specialty_description = 'Pain Management'
+	-- and nppes_provider_city = 'NASHVILLE'
+	-- and opioid_drug_flag = 'Y'
+
+
+
+--     b. Next, report the number of claims per drug per prescriber. Be sure to include all 
+--		combinations, whether or not the prescriber had any claims. You should report the npi, 
+-- 		the drug name, and the number of claims (total_claim_count).
+
+-- select nppes_provider_first_name, nppes_provider_last_org_name, npi, drug_name, total_claim_count
+-- from prescriber
+-- cross join drug
+-- left join prescription
+-- using(npi, drug_name)
+-- where prescriber.specialty_description = 'Pain Management'
 -- 	and nppes_provider_city = 'NASHVILLE'
 -- 	and opioid_drug_flag = 'Y'
+-- order by nppes_provider_first_name
 
 
 
---     b. Next, report the number of claims per drug per prescriber. Be sure to include all combinations, whether or not the prescriber had any claims. You should report the npi, the drug name, and the number of claims (total_claim_count).
+--     c. Finally, if you have not done so already, fill in any missing values 
+-- 		for total_claim_count with 0. Hint - Google the COALESCE function.
 
-select drug_name, specialty_description, nppes_provider_city, opioid_drug_flag, npi, drug_name
+-- select nppes_provider_first_name, nppes_provider_last_org_name, npi, drug_name, coalesce(total_claim_count, 0)
+-- from prescriber
+-- cross join drug
+-- left join prescription
+-- using(npi, drug_name)
+-- where prescriber.specialty_description = 'Pain Management'
+-- 	and nppes_provider_city = 'NASHVILLE'
+-- 	and opioid_drug_flag = 'Y'
+-- order by total_claim_count
+
+
+
+-- In this set of exercises you are going to explore additional ways to group and organize the output of a query when using postgres. 
+
+-- For the first few exercises, we are going to compare the total number of claims from Interventional Pain Management Specialists compared to those from Pain Managment specialists.
+
+-- 1. Write a query which returns the total number of claims for these two groups. 
+-- Your output should look like this: 
+
+-- specialty_description         |total_claims|
+-- ------------------------------|------------|
+-- Interventional Pain Management|       55906|
+-- Pain Management               |       70853|
+
+
+-- select specialty_description, sum(total_claim_count) as total_claims
+-- from prescriber
+-- inner join prescription
+-- using(npi)
+-- where specialty_description = 'Interventional Pain Management'
+-- 	or specialty_description = 'Pain Management'
+-- group by specialty_description
+-- order by total_claims
+
+
+-- 2. Now, let's say that we want our output to also include the total number of claims between these two groups. 
+-- Combine two queries with the UNION keyword to accomplish this. Your output should look like this:
+
+-- specialty_description         |total_claims|
+-- ------------------------------|------------|
+--                               |      126759|
+-- Interventional Pain Management|       55906|
+-- Pain Management               |       70853|
+
+select specialty_description, sum(total_claim_count) as total_claims
 from prescriber
-cross join drug
-	where prescriber.specialty_description = 'Pain Management'
-	and nppes_provider_city = 'NASHVILLE'
-	and opioid_drug_flag = 'Y'
+inner join prescription
+using(npi)
+where specialty_description = 'Interventional Pain Management'
+	or specialty_description = 'Pain Management'
+group by specialty_description
+order by total_claims
+UNION
+select 
 
 
 
---     c. Finally, if you have not done so already, fill in any missing values for total_claim_count with 0. Hint - Google the COALESCE function.
+
+-- 3. Now, instead of using UNION, make use of GROUPING SETS (https://www.postgresql.org/docs/10/queries-table-expressions.html#QUERIES-GROUPING-SETS) to achieve the same output.
+
+-- 4. In addition to comparing the total number of prescriptions by specialty, let's also bring in information about the number of opioid vs. non-opioid claims by these two specialties. Modify your query (still making use of GROUPING SETS so that your output also shows the total number of opioid claims vs. non-opioid claims by these two specialites:
+
+-- specialty_description         |opioid_drug_flag|total_claims|
+-- ------------------------------|----------------|------------|
+--                               |                |      129726|
+--                               |Y               |       76143|
+--                               |N               |       53583|
+-- Pain Management               |                |       72487|
+-- Interventional Pain Management|                |       57239|
+
+-- 5. Modify your query by replacing the GROUPING SETS with ROLLUP(opioid_drug_flag, specialty_description). How is the result different from the output from the previous query?
+
+-- 6. Switch the order of the variables inside the ROLLUP. That is, use ROLLUP(specialty_description, opioid_drug_flag). How does this change the result?
+
+-- 7. Finally, change your query to use the CUBE function instead of ROLLUP. How does this impact the output?
+
+-- 8. In this question, your goal is to create a pivot table showing for each of the 4 largest cities in Tennessee (Nashville, Memphis, Knoxville, and Chattanooga), the total claim count for each of six common types of opioids: Hydrocodone, Oxycodone, Oxymorphone, Morphine, Codeine, and Fentanyl. For the purpose of this question, we will put a drug into one of the six listed categories if it has the category name as part of its generic name. For example, we could count both of "ACETAMINOPHEN WITH CODEINE" and "CODEINE SULFATE" as being "CODEINE" for the purposes of this question.
+
+-- The end result of this question should be a table formatted like this:
+
+-- city       |codeine|fentanyl|hyrdocodone|morphine|oxycodone|oxymorphone|
+-- -----------|-------|--------|-----------|--------|---------|-----------|
+-- CHATTANOOGA|   1323|    3689|      68315|   12126|    49519|       1317|
+-- KNOXVILLE  |   2744|    4811|      78529|   20946|    84730|       9186|
+-- MEMPHIS    |   4697|    3666|      68036|    4898|    38295|        189|
+-- NASHVILLE  |   2043|    6119|      88669|   13572|    62859|       1261|
+
+-- For this question, you should look into use the crosstab function, which is part of the tablefunc extension (https://www.postgresql.org/docs/9.5/tablefunc.html). In order to use this function, you must (one time per database) run the command
+-- 	CREATE EXTENSION tablefunc;
+
+-- Hint #1: First write a query which will label each drug in the drug table using the six categories listed above.
+-- Hint #2: In order to use the crosstab function, you need to first write a query which will produce a table with one row_name column, one category column, and one value column. So in this case, you need to have a city column, a drug label column, and a total claim count column.
+-- Hint #3: The sql statement that goes inside of crosstab must be surrounded by single quotes. If the query that you are using also uses single quotes, you'll need to escape them by turning them into double-single quotes.
